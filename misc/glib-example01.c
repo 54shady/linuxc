40b2c068e5f1d58b3fc2c6811961ba2e1e86fcd8
zeroway <M_O_Bz@163.com>
2022-07-14 14:08:49 +0800

    Fix commpile warning message

--- misc/glib-example01.c
+++ misc/glib-example01.c
@@ -1,5 +1,11 @@
 #include <glib.h>
 
+/*
+ * glib bug fix, include the header file manually
+ * which shoud be include by glib.h
+ */
+#include <glib/gprintf.h>
+
 /* gcc  glib-example01.c `pkg-config --cflags --libs glib-2.0` */
 int main(int argc, char *argv[])
 {
@@ -15,7 +21,7 @@ int main(int argc, char *argv[])
 	g_hash_table_insert(hash, "Ohio", "Columbus");
 
 	g_printf("There are %d keys in the hash\n", g_hash_table_size(hash));
-	g_printf("The capital of %s is %s\n", key2, g_hash_table_lookup(hash, key2));
+	g_printf("The capital of %s is %s\n", key2, (char *)g_hash_table_lookup(hash, key2));
 	gboolean found = g_hash_table_remove(hash, key1);
 	g_printf("The value '%s' was %sfound and removed, %d items left\n", key1, found ? "" : "not ", g_hash_table_size(hash));
 	g_hash_table_destroy(hash);
