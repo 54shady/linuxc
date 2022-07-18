1379afb3e2dc6198943a41f656be488386627907
zeroway <M_O_Bz@163.com>
2022-07-18 14:59:30 +0800

    Add segfault(SIGSEGV) debug example

---
+++ misc/segv/a.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*
+ * 1. 编译文件
+ * gcc -g a.c
+ *
+ * 2. 查看内核dmesg信息
+ * segfault at 0 ip 0000563fe3f9a13c sp 00007fffd4ee7e50 error 6 in a.out[563fe3f9a000+1000]
+ * 0000563fe3f9a13c - 563fe3f9a000+1000 = 113c
+ *
+ * 上面的打印信息来自内核的(arch/x86/mm/fault.c)show_signal_msg函数
+ *
+ * 其中arch/x86/include/asm/trap_pf.h文件内容说明了error code用多少位来表示
+ * Page fault error code bits:
+ *
+ *   bit 0 ==    0: no page found   1: protection fault
+ *   bit 1 ==    0: read access     1: write access
+ *   bit 2 ==    0: kernel-mode access  1: user-mode access
+ *   bit 3 ==               1: use of reserved bit detected
+ *   bit 4 ==               1: fault was an instruction fetch
+ *   bit 5 ==               1: protection keys block access
+ *   bit 15 ==              1: SGX MMU page-fault
+ *
+ * 3. 生成反汇编文件,并找到出错位置的代码
+ * objdump -D a.out > a.dis
+ * grep 113c a.dis
+ * 113c:       c7 00 0c 00 00 00       movl   $0xc,(%rax)
+ *
+ * 4. 使用addr2line找到对应到c文件中的代码位置
+ * gcc 编译的时候需要添加-g参数才能看到行数
+ * addr2line -e a.out 113c
+ */
+
+int main(int argc, char *argv[])
+{
+	int *p = NULL;
+
+	*p = 12;
+
+	return 0;
+}
---
+++ misc/segv/c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * gcc -g c.c
+ *
+ * segfault at 561cc2ae2004 ip 0000561cc2ae113f sp 00007ffe0913ba10 error 6 in * a.out[561cc2ae1000+1000]
+ * 0000561cc2ae113f - 561cc2ae1000+1000 = 113f
+ *
+ * objdump -D a.out > a.dis
+ * grep 113f a.dis
+ * 113f:       c7 00 54 65 73 74       movl   $0x74736554,(%rax)
+ *
+ * addr2line -e a.out 113f
+ * /path/to/c.c:21
+ *
+ * 可能是由于编译器开启了pie功能(--enable-default-pie)
+ * 导致同样代码在不同内核上会看到如下错误
+ * segfault at 400590 ip 00000000004004e4 sp 00007fffa24d72a0 error 7 in * a.out[400000+1000]
+ *
+ * grep 4004e4 a.dis
+ *   4004e4:       c7 00 54 65 73 74       movl   $0x74736554,(%rax)
+ *
+ * addr2line -e a.out 4004e4
+ */
+
+int main(int argc, char *argv[])
+{
+	char *p = "test";
+
+	strcpy(p, "Test");
+
+	return 0;
+}
